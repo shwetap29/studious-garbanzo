9e968bd6e98dad28a4c4dd283af6a97da383218b
shwetap29 <shwetaprasad48@gmail.com>
2021-05-07 11:26:58 +0530

    added add function in calculation.sh

--- calculation.sh
+++ calculation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
-
 echo "welcome to calculation"
+
+function add() {
+         local a=$1
+         local b=$2
+         local c=$(($a + $b ))
+         echo $c
+}
+a=15
+b=13
+result="$(add $a $b)"
+echo $result
